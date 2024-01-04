a3df70dda3ddf3c4c11cd1d00b6514b75e2b8ee7
Ryan Rafael <ryrafael461@gmail.com>
2024-01-04 14:10:29 -0300

    StatusPedido enum criado

--- src/main/java/com/educandoweb/course/configuracao/ConfiguracaoTeste.java
+++ src/main/java/com/educandoweb/course/configuracao/ConfiguracaoTeste.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entidades.Pedido;
 import com.educandoweb.course.entidades.Usuario;
+import com.educandoweb.course.entidades.enums.StatusPedido;
 import com.educandoweb.course.repositorios.RepositorioPedido;
 import com.educandoweb.course.repositorios.RepositorioUsuario;
 
@@ -29,9 +30,9 @@ public class ConfiguracaoTeste implements CommandLineRunner{
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Pedido o1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Pedido o2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Pedido o3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Pedido o1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), StatusPedido.PAGO, u1);
+		Pedido o2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), StatusPedido.AGUARDANDO_PAGAMENTO, u2);
+		Pedido o3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), StatusPedido.AGUARDANDO_PAGAMENTO, u1);
 		
 		
 		repositorioUsuario.saveAll(Arrays.asList(u1,u2));
--- src/main/java/com/educandoweb/course/entidades/Pedido.java
+++ src/main/java/com/educandoweb/course/entidades/Pedido.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entidades.enums.StatusPedido;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -14,51 +15,72 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Pedido implements Serializable{
+public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant momento;
-	
+
+	private Integer statusPedido;
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Usuario cliente;
-	
-	public Pedido() {}
-	public Pedido(Long id, Instant momento, Usuario cliente) {
+
+	public Pedido() {
+	}
+
+	public Pedido(Long id, Instant momento, StatusPedido statusPedido, Usuario cliente) {
 		super();
+		setStatusPedido(statusPedido);
 		this.id = id;
 		this.momento = momento;
 		this.cliente = cliente;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Instant getMomento() {
 		return momento;
 	}
+
 	public void setMomento(Instant momento) {
 		this.momento = momento;
 	}
+
+	public StatusPedido getStatusPedido() {
+		return StatusPedido.valueOf(statusPedido);
+	}
+
+	public void setStatusPedido(StatusPedido statusPedido) {
+		if (statusPedido != null) {
+			this.statusPedido = statusPedido.getCodigo();
+		}
+	}
+
 	public Usuario getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Usuario cliente) {
 		this.cliente = cliente;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -70,6 +92,5 @@ public class Pedido implements Serializable{
 		Pedido other = (Pedido) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
